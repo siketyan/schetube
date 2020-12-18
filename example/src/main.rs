8acebbda86c34f0b02aaca366d88d14492e1449d
Naoki Ikeguchi <root@siketyan.dev>
2020-12-18 22:54:46 +0900

    📝 Add example project

---
+++ example/src/main.rs
@@ -0,0 +1,30 @@
+use chrono::Local;
+use std::env::args;
+use std::error::Error;
+
+use schetube::fetch_upcoming_videos;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let arguments = args().collect::<Vec<_>>();
+    if arguments.len() < 2 {
+        return Ok(println!(
+            "Usage: {} [Channel ID]",
+            arguments.get(0).unwrap_or(&String::from("./schetube"))
+        ));
+    }
+
+    let channel_id = arguments.get(1).unwrap();
+    let videos = fetch_upcoming_videos(channel_id).await?;
+
+    for video in videos {
+        println!(
+            "{}: {} ({})",
+            video.id,
+            video.title,
+            video.scheduled_at.with_timezone(&Local)
+        );
+    }
+
+    Ok(())
+}
